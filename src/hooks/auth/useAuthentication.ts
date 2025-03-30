43293c52ccd087d52e362a9c82a3a38a6ddbebe7
hararec-dev <hararecdev@gmail.com>
2025-03-29 21:16:55 -0600

    refactor: se refactoriza el hook 'useAuthentication'

--- src/hooks/auth/useAuthentication.ts
+++ src/hooks/auth/useAuthentication.ts
@@ -9,22 +9,30 @@ export const useAuthentication = (handleFailedAttempt?: () => void) => {
 
     const generateRandomPayload = useCallback((): string => {
         const array = new Uint8Array(AUTH_CONFIG.PAYLOAD_LENGTH);
+        // @ts-ignore - React Native global type declaration
         if (global.crypto?.getRandomValues) {
             global.crypto.getRandomValues(array);
             const timestamp = new Date().getTime();
             const performanceMark = global.performance?.now() || 0;
             for (let i = 0; i < array.length; i++) {
+                // eslint-disable-next-line no-bitwise
                 array[i] = array[i] ^
+                    // eslint-disable-next-line no-bitwise
                     ((timestamp >> (i % 8)) & 0xFF) ^
+                    // eslint-disable-next-line no-bitwise
                     ((performanceMark * 1000 >> (i % 8)) & 0xFF);
             }
         } else {
             const timeStamp = new Date().getTime();
             for (let i = 0; i < array.length; i++) {
                 const random = Math.random();
+                // eslint-disable-next-line no-bitwise
                 const timeFragment = (timeStamp >> (i % 8)) & 0xFF;
+                // eslint-disable-next-line no-bitwise
                 const iterationNoise = (i * 13) & 0xFF;
+                // eslint-disable-next-line no-bitwise
                 array[i] = Math.floor(
+                    // eslint-disable-next-line no-bitwise
                     (random * 256) ^ timeFragment ^ iterationNoise
                 ) & 0xFF;
             }
