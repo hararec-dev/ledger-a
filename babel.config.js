c47345af864310a29ac38b0fb2f19907294c4120
hararec-dev <hararecdev@gmail.com>
2025-04-24 22:16:41 -0600

    docs: se modifca documentación de tecnologías usadas

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }]
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
   ],
 };
